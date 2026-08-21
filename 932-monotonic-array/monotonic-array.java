4d1c651707d9701e3f8f19c9dabc051192f6972b
VIPULPANDEY1717 <vipulpandey1041@gmail.com>
2026-08-21 23:08:54 +0530

    Time: 1 ms (95.92%) | Memory: 84.9 MB (90.86%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -1,26 +1,23 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
         int n = nums.length;
-        boolean incr = true;
-        boolean decre = true;
 
-        for(int i=0;i<n-1;i++){
-            if(nums[i] < nums[i+1]){
-                decre = false;
-            }
+        if (nums[0] < nums[n - 1]) {
+            for (int i = 1; i < n; i++)
+                if (nums[i] < nums[i - 1])
+                    return false;
+        }
+        else if (nums[0] > nums[n - 1]) {
+            for (int i = 1; i < n; i++)
+                if (nums[i] > nums[i - 1])
+                    return false;
+        }
+        else {
+            for (int i = 1; i < n; i++)
+                if (nums[i] != nums[0])
+                    return false;
         }
 
-        for(int i=0;i<n-1;i++){
-            if(nums[i] > nums[i+1]){
-                incr = false;
-            }
-        }
-
-        if(incr == true || decre == true){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return true;
     }
 }
